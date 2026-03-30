1daa46f159379dae12d3984122c94ee2f1329e8b
Joshua Porter <joshuaporter.dev@outlook.com>
2026-03-30 14:05:46 -0300

    Add layouts handler

--- src/server.js
+++ src/server.js
@@ -1,17 +1,28 @@
 const path = require('path');
-const express = require("express");
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './web/views'));
 
+/*app.use(expressLayouts);
+app.set('layout', 'layouts/main');
+
+app.use(express.static('src/web/public'));*/
+
+/*app.get('/', (req, res) => {
+  res.render('pages/checkin', {
+    title: 'Check-in',
+  });
+});*/
 
 app.get('/', (req, res) => {
-  res.render('index');
-})
+  res.render('index', {
+    title: 'Check-in',
+  });
+});
 //app.use(express.static('public'))
 
-
 app.listen(3000);
-
-

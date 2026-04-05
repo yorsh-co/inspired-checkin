850db6fd374506dee6525736dadbd950daa4fc90
Josh Porter <joshuaporter.dev@outlook.com>
2026-04-05 08:41:54 -0300

    Update 1 file: qr.service.js

--- src/modules/qr/qr.service.js
+++ src/modules/qr/qr.service.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import { env } from '../config/env.js';
+import { env } from '../../config/env.js';
 
 // Generate QR token (used when creating QR codes)
 const generate = ({ eventId }) => {

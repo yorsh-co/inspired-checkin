6579ee6ab2ba2079edceb660c09f1e7db249362c
Josh Porter <joshuaporter.dev@outlook.com>
2026-04-05 06:47:40 -0300

    wip

--- src/web/routes/web.routes.js
+++ src/web/routes/web.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { requireWebAuth } from './middleware/auth.middleware.js';
+import { requireWebAuth } from '../../middleware/auth.middleware.js';
 
 const router = express.Router();
 

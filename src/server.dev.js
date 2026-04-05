fde4d8a6cabe90a83065adb00ab3ac2a8e76613d
Josh Porter <joshuaporter.dev@outlook.com>
2026-04-04 21:19:00 -0300

    wip: env

--- src/server.dev.js
+++ src/server.dev.js
@@ -1,7 +1,7 @@
 import './config/env.js';
-
 import app from './app.js';
+import { env } from './config/env.js';
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(env.port || 3000, () => {
   console.log('Server running');
 });

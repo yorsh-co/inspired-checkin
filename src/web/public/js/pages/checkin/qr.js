c1b40fad52dad853c85925d53a14f276a3673cee
Josh Porter <joshuaporter.dev@outlook.com>
2026-04-05 13:40:54 -0300

    Update 1 file: qr.js

--- src/web/public/js/pages/checkin/qr.js
+++ src/web/public/js/pages/checkin/qr.js
@@ -19,11 +19,29 @@ export const verifyEventQr = async (qrCode, hintDiv) => {
     // server request TODO:
     const res = await api.checkin.verifyQr(qrCode);
 
-    if (res.ok) {
+    // handle server response
+    if (res.data.checkinComplete) {
+      // go to the app
       ui.showHint(hintDiv, 'QR code ok! 🎉');
+
       await utils.sleep(800);
 
       runSuccessFlow(document.querySelector('[data-checkin="qr-step"]'));
+      // TODO: handle failures
+    } else if (res.data.qrValidated) {
+      // TODO: if the ticket is valid, prompt for qr validation
+      ui.showHint(hintDiv, 'QR ok! Valide seu ingresso 🎟️');
+      await utils.sleep(1200);
+
+      // FIXME:
+      ui.transitionToTicket(
+        document.querySelector('[data-checkin="qr-step"]'),
+        document.querySelector('[data-checkin="ticket-step"]')
+      );
+    } else {
+      // TODO: if the ticket is invalid, return to the input
+      // ui.showError(hintDiv, 'QR inválido 😕 Tenta de novo');
+      throw new Error('Invalid');
     }
   } catch (err) {
     console.error(err);

3843d17caab660041ab2d3672f15791830c93a8f
Joshua Porter <joshuaporter.dev@outlook.com>
2026-04-11 09:14:48 -0300

    remove unused code

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -3,8 +3,5 @@ import * as controller from './auth.controller.js';
 
 const router = express.Router();
 
-router.get('/start', controller.start);
-router.get('/scan', controller.scan);
-router.post('/validate-ticket', controller.validateTicket);
 
 export default router;

3ad973edd6477d20b231f504c3ac3c951df71ad2
Josh Porter <joshuaporter.dev@outlook.com>
2026-04-14 00:09:14 -0300

    Update 1 file: navigation.js

--- src/web/public/js/pages/user/checkin/ui/navigation.js
+++ src/web/public/js/pages/user/checkin/ui/navigation.js
@@ -47,7 +47,7 @@ export const goToStep = async (nextStepKey, options = {}) => {
 
     const container = pageDom.main.container;
     
-    container.style.min-height = container.offsetHeight + 'px';
+    container.style.height = container.offsetHeight + 'px';
 
     await ui.transition.step(nextStep.el, isSameStep ? null : currentStep?.el, {
       delay: skeleton ? 0 : 300
@@ -57,12 +57,12 @@ await utils.sleep(100);
 container.addEventListener(
       'transitionend',
       () => {
-        container.style.min-height = 'auto';
+        container.style.height = 'auto';
       },
       { once: true }
     );
     const nextHeight = container.scrollHeight;
-    container.style.min-height = nextHeight + 'px';
+    container.style.height = nextHeight + 'px';
 
     store.setState({ currentStepKey: nextStepKey, isSkeleton: skeleton });
 

ad1ff7faed71f08cf99f1e95f11bb2e554899497
Josh Porter <joshuaporter.dev@outlook.com>
2026-04-13 23:30:35 -0300

    Update 2 files: boot.css, ticket.step.js

--- src/web/public/js/pages/user/checkin/steps/ticket.step.js
+++ src/web/public/js/pages/user/checkin/steps/ticket.step.js
@@ -61,7 +61,7 @@ export const onTicketInput = async (fromPaste = false) => {
     input.blur();
     input.disabled = true;
     
-    await utils.sleep(300);
+    await utils.sleep(800);
 
     await goToStep('verification', { skeleton: true });
 
